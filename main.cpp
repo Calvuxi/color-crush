c356487a6e7c124dd8f2d3ba70aac5f7208fb283
Calvuxi <calvuxi@gmail.com>
2014-03-25 22:27:31 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 /* --------------- AUTHORS ---------------
-- David PÈrez (hallaplay835@gmail.com).
-- Iv·n Calvo (calvuxi@gmail.com).
+- David P√©rez (hallaplay835@gmail.com).
+- Iv√°n Calvo (calvuxi@gmail.com).
 */
+
+// TO DO
+// - declarar constantes a modo de alias sobre los char usados para imprimir el tablero
+// - chcp?
+// - BACKGROUND_GREEN | BACKGROUND_RED | BACKGROUND_BLUE | BACKGROUND_INTENSITY
 
 // #### Libraries ####
 #include <iostream>
@@ -9,9 +14,9 @@
 #include <cstdlib>
 #include <ctime>
 #include <string>
-// #include <fstream>
+#include <fstream>
 #include <iomanip>
-// #include <climits>
+#include <climits>
 #include <cmath>
 
 using namespace std;
@@ -20,15 +25,16 @@ typedef unsigned short int usi;
 // #### Constants ####
 const string PROMPT = "", INVALID_TYPE = "ERROR. Invalid input data type.", ATTEMPTS = "Attempts", SCORE = "Score";
 const usi DIM = 5, MIN_USER_LENGTH = 5, MAX_USER_LENGTH = 20, MAX_TRIES = 30, CHIP_WIDTH = 8, LEFT_PADDING = 3;
-// const string HELP_FILE = "help.txt", USER_FILE = "users.txt";
+const string HELP_FILE = "help.txt", USER_FILE = "users.txt";
 const bool DEBUG = false;
 
 // #### typedef declarations ####
 typedef enum tChip {magenta, yellow, blue, green, none};
+typedef enum tDirection {moveup, movedown, moveleft, moveright}; // We all love One Direction
 
 typedef struct {
-	tChip a[DIM][DIM];
-	usi dim;
+tChip a[DIM][DIM];
+usi dim;
 } tBoard;
 
 typedef struct {
@@ -38,10 +44,10 @@ typedef struct {
 } tGame;
 
 typedef enum tColor {
-	black, dark_blue, dark_green, dark_cyan, dark_red,
-	dark_magenta, dark_yellow, light_gray, dark_gray,
-	light_blue, light_green, light_cyan, light_red,
-	light_magenta, light_yellow, white
+black, dark_blue, dark_green, dark_cyan, dark_red,
+dark_magenta, dark_yellow, light_gray, dark_gray,
+light_blue, light_green, light_cyan, light_red,
+light_magenta, light_yellow, white
 };
 
 // #### Prototypes ####
@@ -54,6 +60,19 @@ usi countDigits(unsigned int n);
 usi menu();
 tGame newGame(usi dim = DIM, usi max_tries = MAX_TRIES);
 void genBoard(tBoard &board);
+
+bool inBounds(const tGame& game, int row, int col);
+void conductGame(tGame& game);
+bool getValidMove(const tGame& game, int& row, int& col, tDirection& dir);
+bool testGroup(const tGame& game, int row, int col, tDirection dir, tChip color);
+bool testValidMove(const tGame& game, int row, int col, tDirection dir);
+void processTurn(tGame& game);
+bool seekAndDestroy(tGame& game);
+bool drop(tGame& game);
+void dropLoop(tGame& game);
+
+bool promptAction(int dim, int& row, int& col, tDirection& dir);
+bool pausar();
 
 void displayGame(tGame &game, usi chip_width = CHIP_WIDTH, usi left_padding = LEFT_PADDING);
 void printRow(usi i, tBoard &board, usi left_padding = LEFT_PADDING, usi chip_width = CHIP_WIDTH);
@@ -70,7 +89,7 @@ int main() {
 
 	// Initialize seed:
 	srand((unsigned int)time(NULL));
-	
+
 	// Default config:
 	usi max_tries = MAX_TRIES;
 	usi chip_width = CHIP_WIDTH;
@@ -78,7 +97,7 @@ int main() {
 
 	// Ask for and validate a username:
 	/*string user = getUserName();
-	cout << "°Hello " << user << "! Choose an option..." << endl;*/
+	cout << "¬°Hello " << user << "! Choose an option..." << endl;*/
 
 	// Display the menu and read an option:
 	usi opt = menu();
@@ -88,7 +107,6 @@ int main() {
 		case 1:
 			tGame game = newGame(dim, max_tries);
 			displayGame(game, chip_width);
-
 			break;
 		}
 
@@ -136,7 +154,7 @@ bool readBool(string prompt, string opt1, string opt2) {
 	}
 	cin.sync();
 
-	if (i == opt1) return true; else return false;
+	return (i == opt1);
 }
 
 /** Returns the number of digits of an unsigned integer. **/
@@ -182,11 +200,11 @@ void genBoard(tBoard &board) {
 
 /** Displays the board, tries and score of game. **/
 void displayGame(tGame &game, usi chip_width, usi left_padding) {
-	
+
 	// Computes leading whitespace to center header.
 	// left_padding + space + chips + separators.
 	usi x = (left_padding + 1 + chip_width * game.board.dim + game.board.dim + 1);
-	// ATTEMPTS + ": " + score + " / " + SCORE + ": " + tries. 
+	// ATTEMPTS + ": " + score + " / " + SCORE + ": " + tries.
 	usi y = ATTEMPTS.length() + 2 + countDigits(game.score) + 3 + SCORE.length() + 2 + countDigits(game.tries);
 	usi center_space = 0;
 	if (x > y) {
@@ -202,10 +220,10 @@ void displayGame(tGame &game, usi chip_width, usi left_padding) {
 
 	// Board:
 	for (usi i = 0; i < game.board.dim - 1; i++) {
-		
+
 		// Row:
 		printRow(i, game.board, left_padding, chip_width);
-		
+
 		// Separator:
 		printSeparator(game.board.dim, left_padding, chip_width);
 	}
@@ -220,7 +238,7 @@ void displayGame(tGame &game, usi chip_width, usi left_padding) {
 	usi margin = chip_width / 2;
 	usi cw_parity = chip_width % 2; // 0 is even, 1 is odd.
 	cout << setw(left_padding + 2 + margin - (cw_parity == 0 ? 1 : 0)) << ' '; // Centers labels depending on parity of chip_width.
-																			   // Labels are left-aligned if they cannot be centered.
+	// Labels are left-aligned if they cannot be centered.
 	for (usi i = 1; i <= game.board.dim; i++) {
 		cout << i << setw(2 * margin + cw_parity - (countDigits(i) - 1)) << ' ';
 	}
@@ -280,21 +298,28 @@ void setColor(tColor color) {
 
 /** Returns appropiate printing color of a chip. **/
 tColor tChipTotColor(tChip chip) {
-	if (chip == magenta) {
-		return dark_magenta;
-	} else if (chip == yellow) {
-		return dark_yellow;
-	} else if (chip == blue) {
-		return dark_cyan;
-	} else if (chip == green) {
-		return dark_green;
-	} else { // chip == none.
-		return black;
+	tColor resultado;
+	switch (chip) {
+	case magenta:
+		resultado = dark_magenta;
+		break;
+	case yellow:
+		resultado = dark_yellow;
+		break;
+	case blue:
+		resultado = dark_cyan;
+		break;
+	case green:
+		resultado = dark_green;
+		break;
+	default:
+		black;
 	}
+	return resultado;
 }
 
 /** Asks for and returns a username.
- ** Username must be a single word with length between MIN_USER_LENGTH and MAX_USER_LENGTH chars. **/
+** Username must be a single word with length between MIN_USER_LENGTH and MAX_USER_LENGTH chars. **/
 string getUserName() {
 	string user;
 	cout << "Welcome to Color Crush. Please, input a username: ";
@@ -314,3 +339,141 @@ string getUserName() {
 	}
 	return user;
 }
+
+void conductGame(tGame& game) {
+	bool canceled = false;
+	int row, col, row2, col2;
+	tDirection dir;
+	tChip aux;
+	while (!canceled && game.tries > 0) {
+		canceled = getValidMove(game, row, col, dir);
+		if (!canceled) {
+			switch(dir) {
+				case moveup:
+					row2 = row -1;
+					col2 = col;
+					break;
+				case movedown:
+					row2 = row +1;
+					col2 = col;
+					break;
+				case moveleft:
+					row2 = row;
+					col2 = col -1;
+					break;
+				case moveright:
+					row2 = row;
+					col2 = col +1;
+					break;
+			}
+			aux = game.board.a[row][col];
+			game.board.a[row][col] = game.board.a[row2][col2];
+			game.board.a[row2][col2] = aux;
+		}
+		processTurn(game);
+	}
+}
+
+bool getValidMove(const tGame& game, int& row, int& col, tDirection& dir) {
+	bool canceled = promptAction(game.board.dim, row, col, dir);
+	if (!canceled) {
+		col = col -1;
+		row = game.board.dim - row;
+		bool valid = testValidMove(game, row, col, dir);
+		while (!valid && !canceled) {
+			cout << "That's not valid, dude. ";
+			canceled = promptAction(game.board.dim, row, col, dir);
+			if (!canceled) valid = testValidMove(game, row, col, dir);
+		}
+	}
+	return canceled;
+}
+
+bool testGroup(const tGame& game, int row, int col, tDirection dir, tChip color) {
+	int w = 0, a = 0, s = 0, d = 0;
+	bool found = false;
+	bool inBounds = true;
+	while (!found && inBounds && dir != moveup) {
+		inBounds = (row-(w+1) >= 0);
+		if (inBounds) {
+			found = (color == game.board.a[row-(w+1)][col]);
+			if (!found) w++;
+		}
+	}
+	found = false;
+	inBounds = true;
+	while (!found && inBounds && dir != moveleft) {
+		inBounds = (col-(a+1) >= 0);
+		if (inBounds) {
+			found = (color == game.board.a[row][col-(a+1)]);
+			if (!found) a++;
+		}
+	}
+	found = false;
+	inBounds = true;
+	while (!found && inBounds && dir != movedown) {
+		inBounds = (row+(s+1) < game.board.dim);
+		if (inBounds) {
+			found = (color == game.board.a[row+(s+1)][col]);
+			if (!found) s++;
+		}
+	}
+	found = false;
+	inBounds = true;
+	while (!found && inBounds && dir != moveright) {
+		inBounds = (col+(d+1) < game.board.dim);
+		if (inBounds) {
+			found = (color == game.board.a[row][col+(d+1)]);
+			if (!found) d++;
+		}
+	}
+
+	return (a+d+1 >= 3)||(w+s+1 >= 3);
+}
+
+bool testValidMove(const tGame& game, int row, int col, tDirection dir) {
+	bool result;
+	switch (dir) {
+	case moveup:
+		result = inBounds(game, row, col) && inBounds(game, row-1, col);
+		if (result) {
+			result = testGroup(game, row, col, moveup, game.board.a[row-1][col])
+						|| testGroup(game, row, col, movedown, game.board.a[row][col]);
+		}
+		break;
+	case movedown:
+		result = inBounds(game, row, col) && inBounds(game, row+1, col);
+		if (result) {
+			result = testGroup(game, row, col, movedown, game.board.a[row+1][col])
+						|| testGroup(game, row, col, moveup, game.board.a[row][col]);
+		}
+		break;
+	case moveleft:
+		result = inBounds(game, row, col) && inBounds(game, row, col-1);
+		if (result) {
+			result = testGroup(game, row, col, moveleft, game.board.a[row][col-1])
+						|| testGroup(game, row, col, moveright, game.board.a[row][col]);
+		}
+		break;
+	case moveright:
+		result = inBounds(game, row, col) && inBounds(game, row, col+1);
+		if (result) {
+			result = testGroup(game, row, col, moveright, game.board.a[row][col+1])
+						|| testGroup(game, row, col, moveleft, game.board.a[row][col]);
+		}
+	}
+	return result;
+}
+
+bool inBounds(const tGame& game, int row, int col) {
+	return row >= 0 && row < game.board.dim && col >= 0 && col < game.board.dim;
+}
+
+void processTurn(tGame& game) {
+	bool stabilized = seekAndDestroy(game);
+	while (!stabilized) {
+		dropLoop(game);
+		stabilized = seekAndDestroy(game);
+	}
+}
+
